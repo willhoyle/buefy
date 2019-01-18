4544f9b96f24ba6b2129e273472a96b98e68919a
Will Hoyle <will.hoyle@gmail.com>
2019-01-18 15:23:53 +0000

    Hack to fix duplicate headers and empty state

--- src/components/table/TableColumn.vue
+++ src/components/table/TableColumn.vue
@@ -1,75 +1,82 @@
 <template>
-    <td
-        v-if="visible"
-        :class="rootClasses"
-        :data-label="label">
-        <span><slot/></span>
-    </td>
+  <td v-if="visible" :class="rootClasses" :data-label="label">
+    <span>
+      <slot/>
+    </span>
+  </td>
 </template>
 
 <script>
-    export default {
-        name: 'BTableColumn',
-        props: {
-            label: String,
-            customKey: [String, Number],
-            field: String,
-            meta: [String, Number, Boolean, Function, Object, Array, Symbol],
-            width: [Number, String],
-            numeric: Boolean,
-            centered: Boolean,
-            sortable: Boolean,
-            visible: {
-                type: Boolean,
-                default: true
-            },
-            customSort: Function,
-            internal: Boolean // Used internally by Table
-        },
-        data() {
-            return {
-                newKey: this.customKey || this.label
-            }
-        },
-        computed: {
-            rootClasses() {
-                return {
-                    'has-text-right': this.numeric && !this.centered,
-                    'has-text-centered': this.centered
-                }
-            }
-        },
-        methods: {
-            addRefToTable() {
-                if (!this.$parent.$data._isTable) {
-                    this.$destroy()
-                    throw new Error('You should wrap bTableColumn on a bTable')
-                }
-
-                if (this.internal) return
-
-                // Since we're using scoped prop the columns gonna be multiplied,
-                // this finds when to stop based on the newKey property.
-                const repeated = this.$parent.newColumns.some(
-                    (column) => {
-                        let columnNewKey = column.customKey || column.label
-                        return columnNewKey === this.newKey
-                    })
-                !repeated && this.$parent.newColumns.push(this)
-            }
-        },
-        beforeMount() {
-            this.addRefToTable()
-        },
-        beforeUpdate() {
-            this.addRefToTable()
-        },
-        beforeDestroy() {
-            const index = this.$parent.newColumns.map(
-                (column) => column.newKey).indexOf(this.newKey)
-            if (index >= 0) {
-                this.$parent.newColumns.splice(index, 1)
-            }
-        }
+export default {
+  name: "BTableColumn",
+  props: {
+    label: String,
+    customKey: [String, Number],
+    field: String,
+    meta: [String, Number, Boolean, Function, Object, Array, Symbol],
+    width: [Number, String],
+    numeric: Boolean,
+    centered: Boolean,
+    sortable: Boolean,
+    visible: {
+      type: Boolean,
+      default: true
+    },
+    customSort: Function,
+    internal: Boolean // Used internally by Table
+  },
+  data() {
+    return {
+      newKey: this.customKey || this.label
+    };
+  },
+  computed: {
+    rootClasses() {
+      return {
+        "has-text-right": this.numeric && !this.centered,
+        "has-text-centered": this.centered
+      };
     }
+  },
+  methods: {
+    addRefToTable() {
+      if (!this.$parent.$data._isTable) {
+        this.$destroy();
+        throw new Error("You should wrap bTableColumn on a bTable");
+      }
+
+      if (this.internal) return;
+      this.$nextTick(() => {
+        if (this.meta) {
+          let columns = this.$parent.newColumns;
+          let idx = -1;
+          idx = columns.findIndex(element => {
+            return element.label == this.label && !element.meta;
+          });
+          if (idx !== -1) {
+            this.$set(this.$parent.newColumns, idx, this);
+          } else {
+            if (columns.length < this.$parent.columns.length) {
+              this.$parent.newColumns.push(this);
+            }
+          }
+        }
+      });
+    }
+  },
+  beforeMount() {
+    this.addRefToTable();
+  },
+  beforeUpdate() {
+    this.addRefToTable();
+  },
+  beforeDestroy() {
+    const index = this.$parent.newColumns
+      .map(column => column.newKey)
+      .indexOf(this.newKey);
+    if (index >= 0) {
+      this.$parent.newColumns.splice(index, 1);
+    }
+  }
+};
 </script>
